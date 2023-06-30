11b4b5064760ecf81c7fd9a7b68b3320a688bcd0
Der Melchior <thilo.haupt@gmail.com>
2023-06-30 14:46:59 +0200

    modified: html5/index.html html5/css/styles.css html5/js/scripts.js

--- html5/js/scripts.js
+++ html5/js/scripts.js
@@ -7,7 +7,7 @@ document.addEventListener("DOMContentLoaded", function() {
 
     var btnDE = document.getElementById("BtnDE");
     var btnEN = document.getElementById("BtnEN");
-    var SurveyModal = new bootstrap.Modal(document.getElementById("my-modal"));
+    var SurveyModal = document.getElementById("my-modal");
 
     console.log('DOMContentLoaded! ')
     survey.supportedLocales = ["en", "de"];
@@ -24,28 +24,27 @@ document.addEventListener("DOMContentLoaded", function() {
         // Get the button that triggered the modal
         var button = event.relatedTarget;
         // Extract value from the custom data-* attribute
-        document.ostrale_survey_locale = button.getAttribute("data-language");
+        document.ostrale_survey_locale = button.getAttribute("data-bs-locale");
+        console.log('data-bs-locale: ' + button.getAttribute("data-bs-locale"));
         // Change survey language
         //myModal.querySelector(".modal-title").innerText = titleData;
         document.survey.locale = document.ostrale_survey_locale;
         console.log('ostrale_survey_locale: ' + document.ostrale_survey_locale);
         console.log('survey.locale: ' + document.survey.locale);
     });
-/*
-    btnDE.addEventListener("click", function() {SurveyModal.show(); });
-    btnEN.addEventListener("click", function() {SurveyModal.show(); });
-*/
-    
 
-    
+    /*
+        btnDE.addEventListener("click", function() {SurveyModal.show(); });
+        btnEN.addEventListener("click", function() {SurveyModal.show(); });
+    */
+    function call_startSurvey(locale) {
+        document.ostrale_survey_locale = locale;
+        $("#surveyElement").survey({model: survey, locale: locale});
+        console.log('document.locale: ' + document.ostrale_survey_locale);
+        console.log('survey.locale: ' + document.survey.locale);
+
+    }
 
 
 });
 
-function call_startSurvey(locale) {
-    document.ostrale_survey_locale = locale;
-    $("#surveyElement").survey({model: survey, locale: locale});
-    console.log('document.locale: ' + document.ostrale_survey_locale);
-    console.log('survey.locale: ' + document.survey.locale);
-
-}
