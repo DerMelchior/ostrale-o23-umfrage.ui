de842b250961ea96d2033de4a5a5ab0576e9b356
Der Melchior <thilo.haupt@gmail.com>
2023-06-30 20:21:16 +0200

    modified: html5/index.html html5/css/styles.css html5/js/scripts.js

--- html5/js/ostrale23-umfrage.js
+++ html5/js/ostrale23-umfrage.js
@@ -1,19 +1,21 @@
 /* /js/ostrale023-umfrage.js */
+/*
 import {json} from './ostrale023-umfrage_json.js';
 import './survey.jquery.min.js';
+ */
 
-const o23survey = new survey.Model(json);
-o23survey.supportedLocales = ["en", "de"];
-o23survey.defaultLocale = "en";
-o23survey.onComplete.add((sender, options) => {
+
+survey.supportedLocales = ["en", "de"];
+survey.defaultLocale = "en";
+survey.onComplete.add((sender, options) => {
     console.log("DATA: " + JSON.stringify(sender.data, null, 3));
 });
-o23survey.onStarted.add(() => {
-    o23survey.showNavigationButtons = false;
-    o23survey.locale = window.ostrale_survey_locale;
+survey.onStarted.add(() => {
+    survey.showNavigationButtons = false;
+    survey.locale = window.ostrale_survey_locale;
 });
 
-export default {o23survey};
+
 /*
 survey.locale = "de";
 survey.currentLocale = "en";
